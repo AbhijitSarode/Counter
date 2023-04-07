6e1b17f81c0fa2db243a11b62ad854942bc5f03b
AbhijitSarode <arsarode@hotmail.com>
2023-04-07 21:27:01 +0530

    Project completed

--- index.js
+++ index.js
@@ -1,1 +1,63 @@
+let ans = 0
 
+let inputValue
+
+let reset = document.getElementById('reset')
+let para = document.querySelector('p')
+let input = document.querySelector('input')
+let number1 = document.getElementById('incrementByOne')
+let number2 = document.getElementById('decrementByOne')
+let number5 = document.getElementById('incrementByFive')
+let number10 = document.getElementById('decrementByTen')
+let increment = document.getElementById('increment')
+let decrement = document.getElementById('decrement')
+
+// Get value from user
+input.addEventListener('change', function () {
+    inputValue = Number(input.value)
+
+})
+
+// Increment by user input
+increment.addEventListener('click', function () {
+    ans = ans + inputValue
+    para.textContent = ans
+})
+
+// Decrement by user input
+decrement.addEventListener('click', function () {
+    ans = ans - inputValue
+    para.textContent = ans
+})
+
+// Increment by 1
+number1.addEventListener('click', function () {
+    ans = ans + 1
+    para.textContent = ans
+})
+
+// Decrement by 1
+number2.addEventListener('click', function () {
+    ans = ans - 1
+    para.textContent = ans
+})
+
+// Increment by 5
+number5.addEventListener('click', function () {
+    ans = ans + 5
+    para.textContent = ans
+})
+
+// Decrement by 10
+number10.addEventListener('click', function () {
+    ans = ans - 10
+    para.textContent = ans
+})
+
+
+// Reset Button Functionality
+reset.addEventListener('click', function () {
+    ans = 0
+    input.value = ""
+    para.textContent ="0"
+})
